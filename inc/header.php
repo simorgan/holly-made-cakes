56c29f73ea3916c94357f06edaec24ba4616fa5a
Simorgan <simonmorgan1000@hotmail.com>
2021-02-26 21:46:51 +0000

    seo meta tag change

--- inc/header.php
+++ inc/header.php
@@ -9,18 +9,18 @@
     <meta name="apple-mobile-web-app-capable" content="yes">
     <meta name="apple-mobile-web-app-status-bar-style" content="#C4A8D4">
     <!--Bootstrap CSS-->
-    <link rel="bootstrapstylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
     <!--Font Awesome-->
-    <link rel="fontawesomestylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
+    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
     <!--stylesheet--> 
-    <link rel="mainstylesheet" href="style.css">
+    <link rel="stylesheet" href="style.css">
     <!-- jquery CDN -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <!--script sheet-->
     <script src="script.js"></script>
     <?php echo $movingHeader?>
     <!-- google font CDN -->
-    <link href="https://fonts.googleapis.com/css2?family=Dancing+Script&display=swap" rel="googlefontstylesheet">  
+    <link href="https://fonts.googleapis.com/css2?family=Dancing+Script&display=swap" rel="stylesheet">  
     <!-- favicon -->
     <link rel="icon" href="images/whisk.jpg">
     <title><?php echo $title?></title>
