fdcfea8eb7e7ac3bdd79c789b59bfda264445f80
Simorgan <simonmorgan1000@hotmail.com>
2021-03-07 20:49:42 +0000

    links updated

--- inc/header.php
+++ inc/header.php
@@ -49,7 +49,7 @@
   <ul class="navbar-nav">
 <li class="nav-item">
 
-    <a class="nav-link" href="www.hollymadecakes.co.uk/">Home</a>
+    <a class="nav-link" href="#">Home</a>
 </li>
 <li class="nav-item">
     <a id="meet-holly-link" class="nav-link smooth dropdown-hide" href="#meet-holly-anchor">Meet Holly</a>
@@ -57,10 +57,10 @@
 <li class="nav-item  dropdown">
     <a class="nav-link dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false" href="#">Cakes</a>
     <div class="dropdown-menu dropdown-coll">
-      <a id ="celebration-link" class="dropdown-item" href="www.hollymadecakes.co.uk/#cakes-anchor" data-target=".navbar-collapse.show">Celebration Cakes</a>
-      <a id="wedding-cakes-link" class="dropdown-item" href="www.hollymadecakes.co.uk/#wedding-cakes-anchor">Wedding Cakes</a>
-      <a id="cup-cake-link" class="dropdown-item" href="www.hollymadecakes.co.uk/#cup-cakes-anchor">Cup Cakes</a>
-      <a id="tray-bake-link" class="dropdown-item" href="www.hollymadecakes.co.uk/#tray-bake-anchor">Tray Bakes</a>
+      <a id ="celebration-link" class="dropdown-item" href="#/#cakes-anchor" data-target=".navbar-collapse.show">Celebration Cakes</a>
+      <a id="wedding-cakes-link" class="dropdown-item" href="#/#wedding-cakes-anchor">Wedding Cakes</a>
+      <a id="cup-cake-link" class="dropdown-item" href="#/#cup-cakes-anchor">Cup Cakes</a>
+      <a id="tray-bake-link" class="dropdown-item" href="#/#tray-bake-anchor">Tray Bakes</a>
     </div>
 </li>
 <li class="nav-item">
@@ -70,7 +70,7 @@
     <a class="nav-link" href="treat-box.php">The Treat Box</a>
 </li>
 <li class="nav-item">
-    <a class="nav-link dropdown-hide" href="www.hollymadecakes.co.uk/#contact-form-anchor">Contact US</a>
+    <a class="nav-link dropdown-hide" href="#/#contact-form-anchor">Contact US</a>
 </li>
 </ul>
   </div>
