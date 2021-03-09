23dff62755b1f52204570d7605cbe7586fe1832a
Simorgan <simonmorgan1000@hotmail.com>
2021-03-09 11:52:58 +0000

    google search favicon added and links updated

--- inc/header.php
+++ inc/header.php
@@ -10,6 +10,7 @@
     <meta name="apple-mobile-web-app-capable" content="yes">
     <meta name="apple-mobile-web-app-status-bar-style" content="#C4A8D4">
     <link href="https://www.hollymadecakes.co.uk" rel="canonical">
+    <link rel="shortcut icon" href="/images/favicon.png">
     <!--Bootstrap CSS-->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
     <!--Font Awesome-->
@@ -36,7 +37,7 @@
 <div id="wapper" class="wapper">
 <!-- header start -->
 <header class="head-top d-flex justify-content-center">
-<a href="index.php"><img src="images/logo.png" alt="Holly Make Cakes Logo, establised 2018."></a>
+<a href="/"><img src="images/logo.png" alt="Holly Make Cakes Logo, establised 2018."></a>
 </header>
 <!-- header end -->
 <!-- top nav start -->
@@ -49,7 +50,7 @@
   <ul class="navbar-nav">
 <li class="nav-item">
 
-    <a class="nav-link" href="index.php">Home</a>
+    <a class="nav-link" href="/">Home</a>
 </li>
 <li class="nav-item">
     <a id="meet-holly-link" class="nav-link smooth dropdown-hide" href="index.php#meet-holly-anchor">Meet Holly</a>
@@ -57,10 +58,10 @@
 <li class="nav-item  dropdown">
     <a class="nav-link dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false" href="#">Cakes</a>
     <div class="dropdown-menu dropdown-coll">
-      <a id ="celebration-link" class="dropdown-item" href="index.php#cakes-anchor" data-target=".navbar-collapse.show">Celebration Cakes</a>
-      <a id="wedding-cakes-link" class="dropdown-item" href="index.php#wedding-cakes-anchor">Wedding Cakes</a>
-      <a id="cup-cake-link" class="dropdown-item" href="index.php#cup-cakes-anchor">Cup Cakes</a>
-      <a id="tray-bake-link" class="dropdown-item" href="index.php#tray-bake-anchor">Tray Bakes</a>
+      <a id ="celebration-link" class="dropdown-item" href="/#cakes-anchor" data-target=".navbar-collapse.show">Celebration Cakes</a>
+      <a id="wedding-cakes-link" class="dropdown-item" href="/#wedding-cakes-anchor">Wedding Cakes</a>
+      <a id="cup-cake-link" class="dropdown-item" href="/#cup-cakes-anchor">Cup Cakes</a>
+      <a id="tray-bake-link" class="dropdown-item" href="/#tray-bake-anchor">Tray Bakes</a>
     </div>
 </li>
 <li class="nav-item">
@@ -70,7 +71,7 @@
     <a class="nav-link" href="treat-box.php">The Treat Box</a>
 </li>
 <li class="nav-item">
-    <a class="nav-link dropdown-hide" href="index.php#contact-form-anchor">Contact US</a>
+    <a class="nav-link dropdown-hide" href="/#contact-form-anchor">Contact US</a>
 </li>
 </ul>
   </div>
